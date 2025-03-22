6be6a2ecf6eea62b72d3848a303de88f3a20ca10
Mian Ishtiaq <35779127+devbyishtiaq@users.noreply.github.com>
2025-03-22 19:27:39 +0500

    feat: Add ui icons

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,7 @@
+import FiltersIcon from "./filters-icon";
+import SearchIcon from "./search-icon";
+import ChevronDown from "./chevron-down";
+import RefreshIcon from "./refresh-icon";
+import Logo from "./logo";
+
+export { Logo, FiltersIcon, SearchIcon, ChevronDown, RefreshIcon };

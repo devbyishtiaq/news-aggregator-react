ce7ed1a63c3d183760329e1b7c923fca9f76d277
Mian Ishtiaq <35779127+devbyishtiaq@users.noreply.github.com>
2025-03-22 19:33:15 +0500

    feat: Add user preference provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { UserPreferencesProvider } from "./context/user-preferences-context.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <UserPreferencesProvider>
+      <App />
+    </UserPreferencesProvider>
+  </StrictMode>
+);
